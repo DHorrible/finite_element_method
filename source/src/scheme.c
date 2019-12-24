2d29bf4b44cf7386c3bb5a5f334576d3bc2ace46
Pavel Shipilov <splkmaxdk@gmail.com>
2019-12-24 04:51:51 +0300

    Fix parse

--- source/src/scheme.c
+++ source/src/scheme.c
@@ -42,28 +42,30 @@ static int scheme_parse_conf(Scheme* scheme, FILE* cfg_file) {
 
     int elem_len = 0;
 
+    int general_doff = DOFF_ALL;
+
     while (!feof(cfg_file)) {
         fgets(line, STD_MAX_LEN_CFG_STR, cfg_file);
-        
         if (strstr(line, "elem:")) {
             is_conn = 0;
             scheme->elements_len = ++elem_len;
             scheme->elements = (Element**)realloc(scheme->elements, scheme->elements_len * sizeof(Element*));
             scheme->elements[scheme->elements_len - 1] = element_create();
             if (elem_len > 1) {
-                scheme->elements[scheme->elements_len - 1]->conn_start = scheme->elements[scheme->elements_len - 2]->conn_start;
+                scheme->elements[scheme->elements_len - 1]->conn_start = scheme->elements[scheme->elements_len - 2]->conn_finish;
                 if (elem_len == 2) {
                     scheme->elements[0]->conn_finish = scheme->elements[1]->conn_start; 
                 }
             }
         } else {
-            if (is_conn) {
-                if (strstr(line, "csrt")) {
+            if (strstr(line, "csrt")) {
+                    int csrt = get_cfg_int_val(line);
                     if (is_first_conn) {
-                        update_doff(scheme->elements[scheme->elements_len - 1]->conn_start, get_cfg_int_val(line));
+                        update_doff(scheme->elements[scheme->elements_len - 1]->conn_start, csrt);
                     } else {
-                        update_doff(scheme->elements[scheme->elements_len - 1]->conn_finish, get_cfg_int_val(line));
+                        update_doff(scheme->elements[scheme->elements_len - 1]->conn_finish, csrt);
                     }
+                    set_csrt(&general_doff, csrt);
                 } else if (strstr(line, "fx")) {
                     if (is_first_conn) {
                         scheme->elements[scheme->elements_len - 1]->conn_start->fx = get_cfg_double_val(line);
@@ -99,25 +101,28 @@ static int scheme_parse_conf(Scheme* scheme, FILE* cfg_file) {
                         scheme->elements[scheme->elements_len - 1]->conn_start->mz = get_cfg_double_val(line);
                     } else {
                         scheme->elements[scheme->elements_len - 1]->conn_finish->mz = get_cfg_double_val(line);
-                    }
+                    }    
                 } else if (strstr(line, "l")) {
-                    is_first_conn = is_conn == 0 ? 1 : 0;
+                    is_first_conn = !is_conn && is_first_conn ? 1 : 0;
                     is_conn = 0;
                     scheme->elements[scheme->elements_len - 1]->length = get_cfg_double_val(line);
                 } else if (strstr(line, "s")) {
-                    is_first_conn = is_conn == 0 ? 1 : 0;
+                    is_first_conn = !is_conn && is_first_conn ? 1 : 0;
                     is_conn = 0;
                     scheme->elements[scheme->elements_len - 1]->square = get_cfg_double_val(line);
                 } else if (strstr(line, "e")) {
-                    is_first_conn = is_conn == 0 ? 1 : 0;
+                    is_first_conn = !is_conn && is_first_conn ? 1 : 0;
                     is_conn = 0;
                     scheme->elements[scheme->elements_len - 1]->hardness = get_cfg_double_val(line);
+                } else if (strstr(line, "conn:")) {
+                    is_first_conn = is_conn ? 0 : is_first_conn;
+                    is_conn = 1;
                 }
-            } else if (strstr(line, "conn:")) {
-                is_conn = 1;
-            }
         }
     }
+
+    assert_msg(general_doff == 0, "Can't solve");
+
     free(line);
 
     return 0;
